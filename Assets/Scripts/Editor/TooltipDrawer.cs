cf3ab57389a464ee346415694889f86b7ee4fb2f
Tuan Le <tuancle@ucsb.edu>
2026-03-27 17:02:51 -0700

    Added Comments to StarryBackground Shader

--- Assets/Scripts/Editor/TooltipDrawer.cs
+++ Assets/Scripts/Editor/TooltipDrawer.cs
@@ -1,7 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 
-// Adds tooltips to ShaderLab properties via [Tooltip(Your Text)]
+// Adds tooltips to ShaderLab properties via [Tooltip(Text)]
 public class TooltipDrawer : MaterialPropertyDrawer
 {
 	private readonly string _tooltip;
@@ -18,11 +18,8 @@ public class TooltipDrawer : MaterialPropertyDrawer
 
 	public override void OnGUI(Rect position, MaterialProperty prop, string label, MaterialEditor editor)
 	{
-		// 1. Draw the native property exactly how Unity normally would (handles any type: Color, Vector, Float, Textures)
 		editor.DefaultShaderProperty(position, prop, label);
 
-		// 2. Overlay a completely invisible label that only contains the tooltip text!
-		// We constrain the width to the name label so the tooltip doesn't trigger when hovering over the value field itself.
 		Rect tooltipRect = position;
 		tooltipRect.width = EditorGUIUtility.labelWidth;
 		GUI.Label(tooltipRect, new GUIContent("", _tooltip));
