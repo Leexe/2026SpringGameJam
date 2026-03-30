2795afa366259318908418ef4ca4f5dbcbb2eeab
Michael Wu <michael_wu@ucsb.edu>
2026-03-29 22:30:23 -0700

    add repair shockwave circle

--- Assets/Scripts/Gameplay/PlayerVisuals.cs
+++ Assets/Scripts/Gameplay/PlayerVisuals.cs
@@ -16,6 +16,9 @@ public class PlayerVisuals : MonoBehaviour
 	private VisualEffect _vfx;
 
 	[SerializeField]
+	private ShockwaveController _shockwave;
+
+	[SerializeField]
 	private AnimancerComponent _animancer;
 
 	[Header("Animations")]
@@ -196,5 +199,6 @@ public class PlayerVisuals : MonoBehaviour
 	private void PlayFullyRepairedVFX()
 	{
 		_vfx.SendEvent("OnFullyRepaired");
+		_shockwave.Pulse();
 	}
 }
